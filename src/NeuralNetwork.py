559e4ebac8e79eb63b775b5d4e01c999854f934b
Ivans Kravcevs <I.Kravcevs@student.tudelft.nl>
2023-02-21 10:07:48 +0100

    First_commit

---
+++ src/NeuralNetwork.py
@@ -0,0 +1,17 @@
+# Note: you are free to organize your code in the way you find most convenient.
+# However, make sure that when your main notebook is run, it executes the steps indicated in the assignment.
+
+class Perceptron:
+
+    def __init__(self):
+        self.w = 0
+        self.b = 0
+        pass
+
+    def evaluate(self, activation_function):
+        return acti
+
+
+class ANN:
+    def __init__(self):
+        pass
