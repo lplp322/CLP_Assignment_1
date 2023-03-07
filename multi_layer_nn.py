c5dd75bf4f11f0b95051506e46aef6c284cf2ce9
Ivans Kravcevs <I.Kravcevs@student.tudelft.nl>
2023-03-07 16:36:51 +0100

    Added one-hidden-layer model and model with multiple possible hidden layers.

--- multi_layer_nn.py
+++ multi_layer_nn.py
@@ -1,7 +1,7 @@
 import numpy as np
 from math import sqrt
-from sklearn.model_selection import train_test_split
-
+# from sklearn.model_selection import train_test_split
+import matplotlib.pyplot as plt
 
 def sigmoid(value):
     if value < 0:
@@ -45,6 +45,7 @@ class Perceptron:
         self.before_activation = None
 
     def compute_weight(self, input_vector):
+        self.input_vector = input_vector
         result = self.weights[-1]  # bias
         for i in range(len(self.weights)-1):
             result += input_vector[i] * self.weights[i]
@@ -52,7 +53,6 @@ class Perceptron:
         return result
 
     def activate(self, input_vector):
-        self.input_vector = input_vector
         value = self.compute_weight(input_vector)
         self.result = self.activation_function(value)
         return self.result
@@ -86,15 +86,18 @@ class ANN:
             input_to_layer = hidden_vector
         result_vector = []
         for perceptron2 in self.output_layer:
-            result_vector.append(perceptron2.activate(input_to_layer))
-        return np.array(result_vector)
+            result_vector.append(perceptron2.compute_weight(input_to_layer))
+        result = np.exp(np.array(result_vector)) / np.sum(np.exp(np.array(result_vector)))
+        for res, perceptron in zip(result, self.output_layer):
+            perceptron.result = res
+        return result
 
     # label is vector with only one 1 in the place of specified class
     def backward_propagate(self, input_vector, labels):
         self.forward_propagate(input_vector)
         errors_output = []
         for i, perceptron in enumerate(self.output_layer):
-            error_rate = (perceptron.result - labels[i])*self.activation_derivative(perceptron.result)
+            error_rate = perceptron.result - labels[i]
             errors_output.append(error_rate)
             perceptron.adjust_weight(error_rate)
         next_layer = self.output_layer
@@ -109,29 +112,159 @@ class ANN:
             errors_output = errors_hidden
             next_layer = layer
 
+def train_test_split(x, y, test_size = 0.15):
+    test_zero = np.zeros(int(len(x)*test_size))
+    dataset = np.concatenate((np.ones(len(x)-len(test_zero)), test_zero))
+    np.random.shuffle(dataset)
+    feature_train = []
+    feature_test =[]
+    target_train = []
+    target_test = []
+    for feature, label, data in zip(x, y, dataset):
+        if data == 1:
+            feature_train.append(list(feature))
+            target_train.append(list(label))
+        else:
+            feature_test.append(list(feature))
+            target_test.append(list(label))
+    return np.array(feature_train), np.array(feature_test), np.array(target_train), np.array(target_test)
 
-if __name__ == '__main__':
-    np.random.seed(0)
+
+class K_fold_split:
+    def __init__(self, x, y, k=5):
+        length = int(len(x) / k)
+        folds = np.zeros(length)
+        for i in range(1, k - 1):
+            fold = np.empty(length)
+            fold.fill(i)
+            folds = np.concatenate((folds, fold))
+        fold_last = np.empty(len(x) - (4 * length))
+        fold_last.fill(k - 1)
+        folds = np.concatenate((folds, fold_last))
+        np.random.shuffle(folds)
+        self.x = x
+        self.y = y
+        self.k = k
+        self.val_num = 0
+        self.folds = folds
+
+    def get_train_validation(self):
+        feature_train = []
+        feature_test = []
+        target_train = []
+        target_test = []
+        for feature, label, data in zip(self.x, self.y, self.folds):
+            if data != self.val_num:
+                feature_train.append(list(feature))
+                target_train.append(list(label))
+            else:
+                feature_test.append(list(feature))
+                target_test.append(list(label))
+        self.val_num = (self.val_num+1) % self.k
+        return np.array(feature_train), np.array(feature_test), np.array(target_train), np.array(target_test)
+
+
+def train(ann, feat_train, target_train):
+    for x, y in zip(feat_train, target_train):
+        ann.backward_propagate(x, y)
+
+
+def validate(ann, feat_test, target_test):
+    result = []
+    error = 0
+    for x, y in zip(feat_test, target_test):
+        res = ann.forward_propagate(x)
+        result.append(res)
+        if res.argmax() != y.argmax():
+            error += 1
+    accuracy = (len(feat_test)-error)/len(feat_test)
+    print("Accuracy: ", accuracy)
+    return accuracy
+# def test_numb_of_epochs(n_epochs):
+#     features = np.loadtxt("./data/features.txt", delimiter=',')
+#     targets = np.loadtxt("./data/targets.txt", dtype='int')
+#
+#     target_vectors = []
+#     for t in targets:
+#         v = np.zeros(7)
+#         v[t - 1] = 1
+#         target_vectors.append(v)
+#     target_vectors = np.array(target_vectors)
+#
+#     feat_train, feat_test, target_train, target_test = train_test_split(features, target_vectors, test_size=0.15)
+#
+#     splitter = K_fold_split(feat_train, target_train)
+#     train_f, validation_f, train_l, validation_l = splitter.get_train_validation()
+#     train_results = []
+#     validation_results = []
+#     n_epochs = 10
+#     for i in range(n_epochs):
+#         train(ann, train_f, train_l)
+#         train_results.append(validate(ann, train_f, train_l))
+#         validation_results.append(validate(ann, validation_f, validation_l))
+#     plt.plot(list(range(n_epochs)), train_results, label='Training')
+#     plt.plot(list(range(n_epochs)), validation_results, label='Validation')
+#     plt.legend()
+#     plt.xlabel('Number of epochs')
+#     plt.ylabel('Accuracy')
+#     plt.title("Train-validation accuracy")
+#     plt.xticks(range(n_epochs))
+#     plt.show()
+def  accuracies_test():
     features = np.loadtxt("./data/features.txt", delimiter=',')
     targets = np.loadtxt("./data/targets.txt", dtype='int')
-    ann = ANN(10, [10, 14], 7, sigmoid, sigmoid_derivative, learning_rate = 0.2)
 
     target_vectors = []
     for t in targets:
         v = np.zeros(7)
-        v[t-1] = 1
+        v[t - 1] = 1
         target_vectors.append(v)
     target_vectors = np.array(target_vectors)
 
-    feat_train, feat_test, target_train, target_test = train_test_split(features, target_vectors, test_size=0.2)
+    feat_train, feat_test, target_train, target_test = train_test_split(features, target_vectors, test_size=0.15)
+    accuracy_result = []
+    for i in range(10):
+        ann = ANN(10, [32], 7, sigmoid, sigmoid_derivative, learning_rate=0.03)
+        train(ann, feat_train, target_train)
+        accuracy_result.append(validate(ann, feat_test, target_test))
+    plt.bar(list(range(10)), accuracy_result)
+    plt.xlabel("Tries")
+    plt.ylabel("Accuracy")
+    plt.title("Accuracy per try")
+    plt.show()
+
+if __name__ == '__main__':
+
+    features = np.loadtxt("./data/features.txt", delimiter=',')
+    targets = np.loadtxt("./data/targets.txt", dtype='int')
+
+    target_vectors = []
+    for t in targets:
+        v = np.zeros(7)
+        v[t - 1] = 1
+        target_vectors.append(v)
+    target_vectors = np.array(target_vectors)
+
+    feat_train, feat_test, target_train, target_test = train_test_split(features, target_vectors, test_size=0.15)
+    splitter = K_fold_split(feat_train, target_train)
+    neuron_performance = []
+    for i in range(7, 31, 3):
+        ann = ANN(10, [i], 7, sigmoid, sigmoid_derivative, learning_rate=0.01)
+        n_epochs = 5
+
+        for i in range(n_epochs):
+            train_f, validation_f, train_l, validation_l = splitter.get_train_validation()
+            train(ann, feat_train, target_train)
+
+        neuron_performance.append(validate(ann, feat_test, target_test))
+
+    plt.plot(list(range(7, 31, 3)), neuron_performance)
+    plt.xlim(7, 31)
+    plt.xlabel('Number of neurons')
+    plt.ylabel('Accuracy')
+    plt.title("Number of neurons vs accuracy")
+    plt.show()
 
 
-    for x, y in zip(feat_train, target_train):
-        ann.backward_propagate(x, y)
-    error = 0
-    for x, y in zip(feat_test, target_test):
-        res = ann.forward_propagate(x)
-        if res.argmax() != y.argmax():
-            error += 1
-    accuracy = (len(target_test)-error)/len(target_test)
-    print(accuracy)
+
+
